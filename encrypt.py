7463069b24631897e610e2ee99595d0cee0340db
renzhentaxi <renzhentaxibaerde@gmail.com>
2015-11-15 10:40:21 -0500

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,71 +1,68 @@
 from PIL import Image
 import random
 
-
-def getSeed(password):
-    seed = 0;
+def get_seed(password):
+    seed = 0
     for c in password:
         seed += ord(c)
-    return seed;
+    return seed
 
 
-def num2RGB(num, initalRGBA):
-    i = lsb(num,initalRGBA)
-    return initalRGBA[0:i] + (num,) + initalRGBA[i+1:3] + (initalRGBA[3]-i,)
+def num2rgba(num, rgba):
+    i = lsb(num, rgba)
+    return rgba[0:i] + (num,) + rgba[i + 1:3] + (rgba[3] - i,)
 
 
-def RGB2num(rgb):
-    i = 255-rgb[3]
-    return (rgb[i])
+def rgba2num(rgb):
+    i = 255 - rgb[3]
+    return rgb[i]
 
-def lsb(num, RGBA):
-    difference = [(x-num)**2 for x in RGBA[0:3]]
+
+def lsb(num, rgba):
+    difference = [(x - num) ** 2 for x in rgba[0:3]]
     return difference.index(min(difference))
 
-def getLength(pix, width, height):
+
+def get_length(pix, width, height):
     return pix[width - 1, height - 1][0]
 
 
-def getVariables(picture):
-    image = Image.open(picture).convert("RGBA")
-    return image, image.size[0], image.size[1], image.load()
+def get_variables(image):
+    return image.size[0], image.size[1], image.load()
 
 
-def getXYLocs(width, height, length):
+def get_xy_loc(width, height, length):
     return random.sample(xrange(width - 2), length), random.sample(xrange(height - 2), length)
 
+def encrypt(image, msg, password="123456789", output_name="encrypted.png"):
+    random.seed(get_seed(password))
+    if type(image) == str:
+        image = Image.open(image).convert("RGBA")
 
-def encrypt(picture, msg, password="123456789", outputName="encrypted.png"):
-    random.seed(getSeed(password))
-
-    image, width, height, pix = getVariables(picture)
+    width, height, pix = get_variables(image)
     length = len(msg)
-    xLoc, yLoc = getXYLocs(width, height, length)
+    x_loc, y_loc = get_xy_loc(width, height, length)
 
     colors = [ord(c) for c in msg]
 
     for i in xrange(length):
-        x, y, c = xLoc[i], yLoc[i], colors[i]
-        pix[x, y] = num2RGB(c, pix[x, y])
+        x, y, c = x_loc[i], y_loc[i], colors[i]
+        pix[x, y] = num2rgba(c, pix[x, y])
     pix[width - 1, height - 1] = (length, length, length)
-    image.save(outputName, "PNG")
+    image.save(output_name, "PNG")
 
 
-def decrypt(picture, password="123456789"):
-    random.seed(getSeed(password))
+def decrypt(image, password="123456789"):
+    random.seed(get_seed(password))
 
-    image, width, height, pix = getVariables(picture)
-    length = getLength(pix, width, height)
-    xLoc, yLoc = getXYLocs(width, height, length)
+    if type(image) == str:
+        image = Image.open(image).convert("RGBA")
+
+    width, height, pix = get_variables(image)
+    length = get_length(pix, width, height)
+    xLoc, yLoc = get_xy_loc(width, height, length)
 
     msg = ""
     for i in xrange(length):
-        msg += chr(RGB2num(pix[xLoc[i], yLoc[i]]))
+        msg += chr(rgba2num(pix[xLoc[i], yLoc[i]]))
     return msg
-
-
-## test
-
-encrypt("cat.jpg", "a" * 255, outputName="regular.png")
-
-print (decrypt("regular.png"))
