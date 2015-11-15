5696948c0597ae278e096f211f1eeb72c18c9e1d
Dor-Ron <dor.rondel@gmail.com>
2015-11-15 08:00:55 -0500

    added flask error handling

--- app.py
+++ app.py
@@ -15,5 +15,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', text="Hello HackRPI")
 
+########################################
+############ Error Handling ############
+########################################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template("500.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
